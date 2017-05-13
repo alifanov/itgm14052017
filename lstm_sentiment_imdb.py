c01aa76554e44816b16ed4e7a5cd96aa583411c9
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-05-13 12:37:49 +0300

    add lstm stuff

--- lstm_sentiment_imdb.py
+++ lstm_sentiment_imdb.py
@@ -18,18 +18,25 @@ from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.datasets import imdb
 
-max_features = 20000
+max_features = 200 #20000
 maxlen = 80  # cut texts after this number of words (among top max_features most common words)
 batch_size = 32
 
 print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features) # most frequent words
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
+LIMIT = 2000
+
+x_train = x_train[:LIMIT]
+y_train = y_train[:LIMIT]
+origin_x_test = x_test[:LIMIT]
+y_test = y_test[:LIMIT]
+
 print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen) # fill smaller and slice bigger
+x_test = sequence.pad_sequences(origin_x_test, maxlen=maxlen)
 print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
 
@@ -47,9 +54,16 @@ model.compile(loss='binary_crossentropy',
 print('Train...')
 model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=15,
+          epochs=5,
           validation_data=(x_test, y_test))
 score, acc = model.evaluate(x_test, y_test,
                             batch_size=batch_size)
 print('Test score:', score)
 print('Test accuracy:', acc)
+
+print('Example #1')
+print('X:', origin_x_test[0])
+print('y:', y_test[0])
+predicted = model.predict(x_test[0], batch_size=1, verbose=0)
+print('Predicted: ', predicted)
+print('Predicted shape: ', predicted.shape)
